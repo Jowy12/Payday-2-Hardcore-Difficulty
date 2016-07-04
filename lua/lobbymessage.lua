aeffa03bea61ecc3cbf1c76cee916232f93d2440
Jowy12 <rezaaa707@gmail.com>
2016-07-05 00:15:06 +0700

    Update lobbymessage.lua

--- lua/lobbymessage.lua
+++ lua/lobbymessage.lua
@@ -1,7 +1,7 @@
 Hooks:Add("NetworkManagerOnPeerAdded", "NetworkManagerOnPeerAdded_DW", function(peer, peer_id)
 	if Network:is_server() then
 		DelayedCalls:Add("DelayedWarnModBB" .. tostring(peer_id), 2, function()
-			local message = "This is Hardcore lobby, a custom difficulty made for all hardcore heisters out there! Enjoy the game!"
+			local message = "This is Hardcore lobby, a custom difficulty made for all hardcore heisters out there! Enjoy the game!(V.3.3.5)"
 			local peer2 = managers.network:session() and managers.network:session():peer(peer_id)
 			if peer2 then
 				peer2:send("send_chat_message", ChatManager.GAME, message)
