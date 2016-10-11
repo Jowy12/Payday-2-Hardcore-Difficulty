08dd2a6aba574acf27d0b2754411943a3641ccc8
Jowy12 <rezaaa707@gmail.com>
2016-10-12 06:54:40 +0700

    Update playertweakdata.lua

--- lua/playertweakdata.lua
+++ lua/playertweakdata.lua
@@ -1,24 +1,82 @@
 PlayerTweakData = PlayerTweakData or class()
 function PlayerTweakData:_set_easy()
-	self.damage.automatic_respawn_time = 150
+	self.damage.DOWNED_TIME_DEC = 20
+	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
 end
 function PlayerTweakData:_set_normal()
-	self.damage.automatic_respawn_time = 120
+	self.damage.DOWNED_TIME_DEC = 20
+	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
 end
 function PlayerTweakData:_set_hard()
-	self.damage.automatic_respawn_time = 220
-	self.damage.DOWNED_TIME_DEC = 7
-	self.damage.DOWNED_TIME_MIN = 5
+	self.damage.DOWNED_TIME_DEC = 20
+	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
 end
 function PlayerTweakData:_set_overkill()
-	self.damage.DOWNED_TIME_DEC = 10
-	self.damage.DOWNED_TIME_MIN = 5
+	self.damage.DOWNED_TIME_DEC = 20
+	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
 end
 function PlayerTweakData:_set_overkill_145()
-	self.damage.DOWNED_TIME_DEC = 15
+	self.damage.DOWNED_TIME_DEC = 20
 	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
+end
+function PlayerTweakData:_set_easy_wish()
+	self.damage.DOWNED_TIME_DEC = 20
+	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
 end
 function PlayerTweakData:_set_overkill_290()
+	self.damage.DOWNED_TIME_DEC = 20
+	self.damage.DOWNED_TIME_MIN = 1
+	self.suspicion.max_value = 12
+	self.suspicion.range_mul = 1.4
+	self.suspicion.buildup_mul = 1.4
+	self.damage.BLEED_OT_TIME = 10
+	self.damage.LIVES_INIT = 4
+	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	self.damage.REVIVE_HEALTH_STEPS = {0.1}
+end
+function PlayerTweakData:_set_sm_wish()
 	self.damage.DOWNED_TIME_DEC = 20
 	self.damage.DOWNED_TIME_MIN = 1
 	self.suspicion.max_value = 12
